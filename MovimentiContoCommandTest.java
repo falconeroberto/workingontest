0e50a63fc1219d75e07e5f61378a4b020aa5c200
falconeroberto <50367117+falconeroberto@users.noreply.github.com>
2022-02-17 17:21:20 +0100

    Add files via upload

---
+++ MovimentiContoCommandTest.java
@@ -0,0 +1,11 @@
+package com.intesasanpaolo.bear.idsc0.command;
+
+import com.intesasanpaolo.bear.idsc0.BaseTest;
+import org.junit.Test;
+
+public class MovimentiContoCommandTest extends BaseTest {
+
+    @Test
+    public void doExecute() {
+    }
+}
---
+++ RicercaRapportiCollegatoCommandTest.java
@@ -0,0 +1,11 @@
+package com.intesasanpaolo.bear.idsc0.command;
+
+import com.intesasanpaolo.bear.idsc0.BaseTest;
+import org.junit.Test;
+
+public class RicercaRapportiCollegatoCommandTest extends BaseTest {
+
+    @Test
+    public void doExecute() {
+    }
+}
